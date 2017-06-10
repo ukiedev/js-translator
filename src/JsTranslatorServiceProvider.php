446428619e6d46011599ad3d7c32f420131a04da
ukiedev <developer@ukietech.com>
2017-06-10 15:30:37 +0300

    Update JsTranslatorServiceProvider.php

--- src/JsTranslatorServiceProvider.php
+++ src/JsTranslatorServiceProvider.php
@@ -25,7 +25,7 @@ class JsTranslatorServiceProvider extends ServiceProvider
 	 */
 	public function register()
 	{
-		$this->app['jstranslator'] = $this->app->share(function ($app)
+		$this->app->singleton('jstranslator', function($app)
 		{
 			$langPath = $app['path.base'].'/resources/lang/' . \App::getLocale();
 
